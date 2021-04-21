76da88346b39fdc76f53c196bfed3011a5604365
Indika Kolamba Tantrege <ikolamba2@gmail.com>
2021-04-21 16:16:31 +0000

    Implement renderTweets and add tweets to page using mock data

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -3,17 +3,31 @@
  * jQuery is already loaded
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
-const tweetData = {
-  "user": {
-    "name": "Newton",
-    "avatars": "https://i.imgur.com/73hZDYK.png",
+
+const data = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png"
+      ,
       "handle": "@SirIsaac"
     },
-  "content": {
+    "content": {
       "text": "If I have seen further it is by standing on the shoulders of giants"
     },
-  "created_at": 1461116232227
-};
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd" },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
 
 const createTweetElement = tweetData => {
   const { name, avatars, handle } = tweetData.user;
@@ -44,8 +58,13 @@ const createTweetElement = tweetData => {
   return $tweet;
 };
 
-const $tweet = createTweetElement(tweetData);
+const renderTweets = tweetDataArr => {
+  tweetDataArr.forEach(tweetData => {
+    const $tweet = createTweetElement(tweetData);
+    $('#container').append($tweet);
+  });
+}
 
 $(document).ready(function () {
-  $('#container').append($tweet);
+  renderTweets(data);
 });
